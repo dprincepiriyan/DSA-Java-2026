a52ffa4a6fbd70fdca4d2c369502b3600d018aa0
dprincepiriyan <princepiriyan07183@gmail.com>
2026-01-12 10:29:22 +0530

    traversal,deleting and finding the lowest value in linked list

--- LINKED_LIST/linked_list.java
+++ LINKED_LIST/linked_list.java
@@ -7,6 +7,34 @@ public class linked_list {
             this.next=null;
         }
     }
+    public static int FindLowest(Node head){
+        int min=head.data;
+        Node curr=head;
+        while(curr!=null){
+            if(curr.data<min){
+                min=curr.data;
+            }
+            curr=curr.next;
+        }
+        return min;
+    }
+    //delete a specific node from linked list
+    public static Node deleteSpecificNode(Node head,Node nodetoDelete){
+        if(head==null ||nodetoDelete==null){
+            return head;
+        }
+        if(head==nodetoDelete){
+            return head.next;
+        }
+        Node curr=head;
+        while(curr!=null && curr.next!=nodetoDelete){
+            curr=curr.next;
+        }
+        if(curr!=null){
+            curr.next=nodetoDelete.next;
+        }
+        return head;
+    }
     public static void main(String[] args) {
         Node firstNode=new Node(10);
         Node secondNode=new Node(20);
@@ -19,7 +47,16 @@ public class linked_list {
         while(currNode!=null){
             System.out.print(currNode.data+" -> ");
             currNode=currNode.next;
-    }
-    System.out.print("null");
+        }
+        System.out.println("Lowest value in the list: " + FindLowest(firstNode));
+        System.out.print("null");
+        System.out.println();
+        firstNode=deleteSpecificNode(firstNode,thirdNode);
+        currNode=firstNode;
+        while(currNode!=null){
+            System.out.print(currNode.data+" -> ");
+            currNode=currNode.next;
+        }
+        System.out.print("null");
     }
 }
